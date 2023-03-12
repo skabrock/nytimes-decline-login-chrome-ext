4236ee93d13545bcd33b14bd7f3e7e6b6fcfa5b9
Solaire <skabrock@gmail.com>
2023-03-12 17:04:36 +0200

    clear inline styles logic

--- content.js
+++ content.js
@@ -1,14 +1,4 @@
-const OVERLAY_DETECTION_ATTEMPTS = 100;
-let overlayDetectionInterval;
 let styleElement;
-let isPageLoaded = false;
-
-function detectBlockerOverlay() {
-  const primeSelector = rules.filter((rule) => rule.prime)[0].selector;
-  const primeElement = document.querySelector(primeSelector);
-
-  return primeElement instanceof Element;
-}
 
 function addCSSRules() {
   const styleElementContent = rules.reduce((css, rule) => {
@@ -33,55 +23,10 @@ function removeCSSRules() {
   }
 }
 
-function camelize(string) {
-  return string.replace(/-./g, (x) => x[1].toUpperCase());
-}
-
-function updateBlockers(value) {
-  const inlineRules = rules.filter((rule) => rule.inline);
-
-  inlineRules.forEach(({ selector, action }) => {
-    const element = document.querySelector(selector);
-
-    if (element instanceof Element) {
-      Object.keys(action).forEach((cssProperty) => {
-        const inlineProperty = camelize(cssProperty);
-        const inlineValue = value !== undefined ? value : action[cssProperty];
-
-        element.style[inlineProperty] = inlineValue;
-      });
-    }
-  });
-}
-
-function restoreBlockers() {
-  updateBlockers(null);
-}
-
 async function getIsActiveState() {
   const { isActiveState } = await chrome.storage.local.get("isActiveState");
 
   return isActiveState !== undefined ? isActiveState : true;
-}
-
-function findBlockersAndClear() {
-  let iteration = 0;
-
-  overlayDetectionInterval = setInterval(() => {
-    // Looking for the main authorization element on the page
-    const overlay = detectBlockerOverlay();
-
-    if (++iteration > OVERLAY_DETECTION_ATTEMPTS) {
-      clearInterval(overlayDetectionInterval);
-      removeCSSRules();
-    }
-
-    if (overlay) {
-      clearInterval(overlayDetectionInterval);
-      // Running the main cleaning function
-      // It uses inline styles and guaranteed cleanup unlike addCSSRules()
-    }
-  }, 100);
 }
 
 // Listening for changes of global state
@@ -89,39 +34,20 @@ function findBlockersAndClear() {
 chrome.storage.onChanged.addListener(function (changes, areaName) {
   if (areaName === "local" && changes.isActiveState) {
     if (changes.isActiveState.newValue) {
-      findBlockersAndClear();
+      addCSSRules();
     } else {
-      // Clearing all traces of the extension work if users has disabled it
-      clearInterval(overlayDetectionInterval);
       removeCSSRules();
-      restoreBlockers();
     }
   }
 });
 
 (async function () {
-  // Add opacity for overlay before it appears
-  // If it doesn't appear, the rules will be removed after max overlay detection attempts are reached
-  // This script doesn't have to wait until the overlay appears unlike updateBlockers()
   addCSSRules();
-
-  window.addEventListener("load", () => {
-    isPageLoaded = true;
-  });
 
   // Get the initial state of the extension
   const isActiveState = await getIsActiveState();
 
-  // Extension could be disabled before opening a target page
-  // Do nothing in this case
   if (!isActiveState) {
     removeCSSRules();
-    return;
-  }
-
-  if (isPageLoaded) {
-    findBlockersAndClear();
-  } else {
-    window.addEventListener("load", findBlockersAndClear);
   }
 })();
--- rules.js
+++ rules.js
@@ -2,18 +2,14 @@ const rules = [
   {
     selector: "#app>div>div:last-child",
     action: { display: "none" },
-    prime: true,
-    inline: true,
   },
   {
     selector: "#app>div>div:first-child>div:last-child",
     action: { display: "none" },
-    inline: true,
   },
   {
     selector: "#app>div>div:first-child",
     action: { "overflow-y": "scroll" },
-    inline: true,
   },
   {
     selector: ".css-mcm29f",
